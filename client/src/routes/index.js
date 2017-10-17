9addba9f016defbef3067c048173136810deff58
mr-moto <khash615@gmail.com>
2017-10-17 01:03:10 -0700

    added Share component/container

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
  
-import {ItemsContainer} from '../containers/Items';
-import Login from '../containers/Login/Login';
-import Profile from '../containers/Profile/ProfileContainer'
+import {ItemsContainer} from '../containers/Items'
+import {Login} from '../containers/Login'
+import {Profile} from '../containers/Profile'
 import {NotFound} from '../containers/NotFound'
+import {ShareContainer} from '../containers/Share'
  
  const Routes = () => (
      <Switch>
          <Route exact path="/" component={ItemsContainer} />
          <Route exact path="/login" component={Login} />
          <Route exact path="/profile/:userid" component={Profile} />
-         <Route exact path="/share" component={Share} />;
+         <Route exact path="/share" component={ShareContainer} />;
          <Route path="*" component={NotFound}/>
      </Switch>
  );

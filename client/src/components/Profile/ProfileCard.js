c4b37b043a3f849bc628f84e6efa7e5bba64f97c
mr-moto <khash615@gmail.com>
2017-10-18 01:37:36 -0700

    styled profile card

--- client/src/components/Profile/ProfileCard.js
+++ client/src/components/Profile/ProfileCard.js
@@ -2,40 +2,18 @@ import React, {Component} from 'react'
 import Paper from 'material-ui/Paper'
 import Gravatar from 'react-gravatar'
 
-// class ProfileCard extends Component {
-//     render() {
-//         const {userData} = this.props
-//         return (
-//             <Paper style={{ maxWidth: '800px', margin: '0 auto' }}>
-//                 <div className="profileContent">
-//                     <div className="nameBio">
-//                         <h2>:(</h2>
-//                         <p></p>
-//                     </div>
-//                     <div className="userMeta">
-//                         <div className="sharedBorrowed">
-//                             <p><span></span> items shared</p>
-//                             <p><span></span> items borrowed</p>
-//                         </div>
-//                         {/* <Gravatar email={userData.email} size={180} className="gravatarImage" /> */}
-//                     </div>
-//                 </div>
-//             </Paper>
-//         );
-//     }
-// }
 const ProfileCard = ({userData}) => {
     return (
         <Paper style={{ maxWidth: '800px', margin: '0 auto' }}>
             <div className="profileContent">
                 <div className="nameBio">
-                    <h2>{userData.fullName}</h2>
-                    <p>{userData.bio}</p>
+                    <h2 className="name">{userData.fullName}</h2>
+                    <p className="bio">{userData.bio}</p>
                 </div>
                 <div className="userMeta">
                     <div className="sharedBorrowed">
-                        <p><span>{userData.shareCount}</span> items shared</p>
-                        <p><span>{userData.borrowCount}</span> items borrowed</p>
+                        <p><span>{userData.shareCount}</span> Items shared</p>
+                        <p><span>{userData.borrowCount}</span> Items borrowed</p>
                     </div>
                     <Gravatar email={userData.email} size={180} className="gravatarImage" />
                 </div>
